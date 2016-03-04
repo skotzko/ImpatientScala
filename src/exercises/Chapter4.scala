0cfebe2a401a6dfe36ddf3f76580d4a249f58535
Andrew Skotzko <andrew.skotzko@gmail.com>
2016-03-04 14:25:56 -0800

    re-do chapter 4 complete

--- src/exercises/Chapter4.scala
+++ src/exercises/Chapter4.scala
@@ -4,6 +4,7 @@ import java.io.File
 import java.util.Scanner
 import scala.collection.immutable.{HashMap, ListMap}
 import io.Source
+import scala.collection.mutable
 
 object Chapter4 extends App {
 
@@ -40,6 +41,8 @@ object Chapter4 extends App {
   print(counts == counts2)
 
   // 4
+  println("\n\nExercise 4")
+
   {
     var scoresByKey = collection.immutable.SortedMap[String, Int]()
     tokens.foreach { t =>
@@ -58,50 +61,73 @@ object Chapter4 extends App {
   println("\n\nExercise 5")
 
   {
-    val scoresByKey = java.util.TreeMap[String, Int]
-    tokens.foreach {t =>
-      val count = scoresByKey.
-      scoresByKey = scoresByKey + (t -> (count + 1))
+    import collection.convert.wrapAsScala.mapAsScalaMap
+    var scoresByKey = new java.util.TreeMap[String, Int]
+    tokens.foreach { t =>
+      val count = scoresByKey.getOrElse(t, 0)
+      scoresByKey += t -> (count + 1)
     }
+
+    println(scoresByKey)
   }
 
-  //
-  //  //5. Repeat the preceding exercise with a java.util.TreeMap that you adapt to the Scala API.
-  //  in = new Scanner(new File(filepath))
-  //  scoresByKey = java.util.TreeMap[String, Int]
-  //  while (in.hasNext) {
-  //    val word = in.next
-  //    val count = scoresByKey.getOrElse(word, 0)
-  //    scoresByKey = scoresByKey + (word -> (count + 1))
-  //  }
-  //
-  //
-  //
-  //
-  //
-  //  //6. Define a linked hash map that maps "Monday" to java.util.Calendar.MONDAY, and similarly for the other weekdays
-  //  // . Demonstrate that the elements are visited in insertion order.
-  //  //7. Print a table of all Java properties, like this:
-  //  //47
-  //  //java.runtime.name sun.boot.library.path java.vm.version java.vm.vendor java.vendor.url path.separator java.vm.name
-  //  //| Java(TM) SE Runtime Environment
-  //  //  | /home/apps/jdk1.6.0_21/jre/lib/i386 | 17.0-b16
-  //  //| Sun Microsystems Inc.
-  //  //| http://java.sun.com/
-  //  //  | :
-  //  //  | Java HotSpot(TM) Server VM
-  //  //You need to find the length of the longest key before you can print the table.
-  //
-  //  //8. Write a function minmax(values: Array[Int]) that returns a pair containing the smallest and largest values in
-  //  // the array.
-  //
+  //  6
+  println("\n\nExercise 6")
+
+  {
+    import java.util.Calendar._
+
+    val days = mutable.LinkedHashMap[String, Int](
+      "Monday" -> MONDAY,
+      "Tuesday" -> TUESDAY,
+      "Wednesday" -> WEDNESDAY,
+      "Thursday" -> THURSDAY,
+      "Friday" -> FRIDAY,
+      "Saturday" -> SATURDAY,
+      "Sunday" -> SUNDAY
+    )
+
+    days.foreach(println)
+
+  }
+
+  // 7
+  println("\n\nExercise 7")
+
+  {
+    import collection.convert.wrapAsScala._
+    val props = propertiesAsScalaMap(System.getProperties)
+    val maxKeyLength = props.keySet.map(_.size).max
+    for ((k, v) <- props) printf("%-" + maxKeyLength + "s | %s\n", k, v)
+  }
+
+
+  // 8
+  println("\n\nExercise 8")
+
+  def minmax(values: Array[Int]) = (values.min, values.max)
+
+  println(minmax(Array(2,1,-2,9,12)))
+
+
+  // 9
+  println("\n\nExercise 9")
+
+  def lteqgt(values: Array[Int], v: Int) = {
+    (values.count(_ < v), values.count(_ == v), values.count(_ > v))
+  }
+
+  val v = 0
+  val output = lteqgt(Array(2,-3,-2,9,1,2,3), v)
+  println(s"Values less than $v: ${output._1}\nValues equal to $v: ${output._2}\nValues greater than $v: ${output._3}")
+
+
+  //  10
+  println("\n\nExercise 10")
+  println("Hello".zip("World"))
+  // hmm... salting passwords? something else crypto-related?
+
   //  //9. Write a function lteqgt(values: Array[Int], v: Int) that returns a triple containing the counts of values less
   //  // than v, equal to v, and greater than v.
-  //
-  //
-  //  //10. What happens when you zip together two strings, such
-  //  // as "Hello".zip("World")? Come up with a plausible use case.
-  //  "Hello".zip("World")
-  //  //generates an indexed sequence of vectors. hmm, use cases...
 
 }
